cccbad62f4286ae13d2cb5b7184e8525d4eedacb
Dzerjinski Lemos <dzerjinski.lemos@zafir.co.ao>
2022-08-29 12:09:04 +0100

    Mon Aug 29 12:09:04 WAT 2022

--- nginx-node/node/index.js
+++ nginx-node/node/index.js
@@ -8,18 +8,57 @@ const config = {
   database: "nodedb",
 };
 const mysql = require("mysql");
-const connection = mysql.createConnection(config);
 
-connection.query("CREATE TABLE IF NOT EXISTS people (name VARCHAR(255))");
+const conn = newConnection();
 
-const sql = `INSERT INTO people(name) values('Dzerjinski')`;
-connection.query(sql);
-connection.end();
+conn.connect();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Full Cycle</h1>");
+createSchema(conn);
+insertPerson(conn);
+
+conn.end();
+
+app.get("/", async (req, res) => {
+  const conn = newConnection();
+  conn.connect();
+
+  const people = await getPeopleFromDb(conn);
+
+  res.send(
+    "<h1>Full Cycle</h1>" +
+      "<p>Lista de nomes cadastrada no banco de dados:</p>" +
+      "<ul>" +
+      people.map((p) => `<li>${p}</li>`).join("") +
+      "</ul>"
+  );
 });
 
 app.listen(port, () => {
   console.log("Rodando na porta " + port);
 });
+
+function insertPerson(conn) {
+  const sql = "INSERT INTO people(name) values('Dzerjinski')";
+  conn.query(sql);
+}
+
+function createSchema(conn) {
+  const sql = "CREATE TABLE IF NOT EXISTS people (name VARCHAR(255))";
+  conn.query(sql);
+}
+
+function getPeopleFromDb(conn) {
+  return new Promise((resolve, reject) => {
+    conn.query("SELECT name FROM people", function (error, results, fields) {
+      if (error) {
+        return reject(error);
+      }
+
+      resolve(results.map((r) => r.name));
+    });
+  });
+}
+
+function newConnection() {
+  return mysql.createConnection(config);
+}
